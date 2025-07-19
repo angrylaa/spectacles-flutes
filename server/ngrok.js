8c071f1621ad3e3faa448f101e8bc4dec248fc9f
angela <adeng32@uwo.ca>
2025-07-19 15:31:22 -0400

    Remove package.json for Firebase functions, update ngrok connection to log auth token, change message-received route to POST, and reorganize route imports in server.js.

--- server/ngrok.js
+++ server/ngrok.js
@@ -1,12 +1,14 @@
+import "dotenv/config";
 import ngrok from "ngrok";
 
 async function startNgrok() {
   try {
-    await ngrok.connect({ authtoken: process.env.NGROK_AUTH_TOKEN });
-    console.log("Ngrok connected successfully");
+    console.log(process.env.NGROK_AUTH_TOKEN);
     const url = await ngrok.connect({
+      authtoken: process.env.NGROK_AUTH_TOKEN,
       addr: 3000,
     });
+
     console.log("Ngrok URL:", url);
   } catch (error) {
     console.error("Error starting Ngrok:", error);
